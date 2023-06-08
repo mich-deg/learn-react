99e11913a30fd8bca1a4dae1ade0148d1079ccc6
Michael <lemichael1215@gmail.com>
2023-06-09 01:13:33 +0300

    rendering lists

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,33 @@
 import React from "react";
 
-const people = [
-  'Creola Katherine Johnson: mathematician',
-  'Mario José Molina-Pasquel Henríquez: chemist',
-  'Mohammad Abdus Salam: physicist',
-  'Percy Lavon Julian: chemist',
-  'Subrahmanyan Chandrasekhar: astrophysicist'
-];
+const people = [{
+  id: 0,
+  name: 'Creola Katherine Johnson',
+  profession: 'mathematician',
+}, {
+  id: 1,
+  name: 'Mario José Molina-Pasquel Henríquez',
+  profession: 'chemist',
+}, {
+  id: 2,
+  name: 'Mohammad Abdus Salam',
+  profession: 'physicist',
+}, {
+  name: 'Percy Lavon Julian',
+  profession: 'chemist',  
+}, {
+  name: 'Subrahmanyan Chandrasekhar',
+  profession: 'astrophysicist',
+}];
 
 const List = () => {
-  const listItems = people.map(person => <li>{person}</li>)
+  const chemists = people.filter(person =>
+    person.profession === 'chemist'
+  );
+  const listItems = chemists.map(person => <li key={person.id}>
+    <b>{person.name}:</b>
+       {' ' + person.profession + ' '}
+  </li>)
   return <ul>{listItems}</ul>
 
 }
