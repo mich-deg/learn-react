9d02ed356de771b5c2cc7d7aa031fefb69f86b86
Michael <lemichael1215@gmail.com>
2023-06-09 00:35:17 +0300

    rendering data from arrays

--- src/App.jsx
+++ src/App.jsx
@@ -1,60 +1,23 @@
 import React from "react";
 
-import PropTypes from "prop-types";
+const people = [
+  'Creola Katherine Johnson: mathematician',
+  'Mario José Molina-Pasquel Henríquez: chemist',
+  'Mohammad Abdus Salam: physicist',
+  'Percy Lavon Julian: chemist',
+  'Subrahmanyan Chandrasekhar: astrophysicist'
+];
 
-// import Gallery from "./component/Gallery";
+const List = () => {
+  const listItems = people.map(person => <li>{person}</li>)
+  return <ul>{listItems}</ul>
 
-const Item = ({ name, isPacked }) => {
-  let itemContent = name;
-  if (isPacked) {
-    itemContent = name + " ✔";
-    // return <li className="item">{name} ✔</li>; // option 1
-    // return null; // option 2
-
-    /* ternary operator --- option 3 */
-    // return (
-    //   <li className="item">{isPacked ? <del>{name + " ✔"}</del> : name}</li>
-    // );
-
-      /* AND operator && --- option 4 */
-
-    // return (
-    //   <li className="item">
-    //     {name} {isPacked && '✔'}
-    //   </li>
-    // );
-  return (
-    <li className="item">
-      {itemContent}
-    </li>
-  );
-  }
-  // return <li className="item">{name}</li>; // with option 1 or 2
-};
-
-Item.propTypes = {
-  name: PropTypes.string.isRequired,
-  isPacked: PropTypes.bool.isRequired,
-};
-
-const PackingList = () => {
-  return (
-    <section>
-      <h1>Sally Rides Packing List</h1>
-      <ul>
-        <Item isPacked={true} name="Space suit" />
-        <Item isPacked={true} name="Helmet with a golden leaf" />
-        <Item isPacked={false} name="Photo of Tam" />
-      </ul>
-    </section>
-  );
-};
+}
 
 const App = () => {
   return (
     <div>
-      {/* <Gallery /> */}
-      <PackingList />
+      <List />
     </div>
   );
 };
