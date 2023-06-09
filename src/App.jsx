9f4134b5ad9fa078f8c6881d7dfaa4b4c6e93c2b
Michael <lemichael1215@gmail.com>
2023-06-09 14:12:25 +0300

    adding event handler

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,13 @@
 import React from "react";
 
-const people = [{
-  id: 0,
-  name: 'Creola Katherine Johnson',
-  profession: 'mathematician',
-}, {
-  id: 1,
-  name: 'Mario José Molina-Pasquel Henríquez',
-  profession: 'chemist',
-}, {
-  id: 2,
-  name: 'Mohammad Abdus Salam',
-  profession: 'physicist',
-}, {
-  name: 'Percy Lavon Julian',
-  profession: 'chemist',  
-}, {
-  name: 'Subrahmanyan Chandrasekhar',
-  profession: 'astrophysicist',
-}];
-
-const List = () => {
-  const chemists = people.filter(person =>
-    person.profession === 'chemist'
-  );
-  const listItems = chemists.map(person => <li key={person.id}>
-    <b>{person.name}:</b>
-       {' ' + person.profession + ' '}
-  </li>)
-  return <ul>{listItems}</ul>
-
-}
-
 const App = () => {
+  const handleClick = () => {
+    alert("You clicked me!")
+  }
   return (
-    <div>
-      <List />
-    </div>
+    <button onClick={handleClick}>
+      Click me
+    </button>
   );
 };
 
