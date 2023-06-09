9b5e02092a2b92a9a12473379da67d99b882cfa1
Michael <lemichael1215@gmail.com>
2023-06-09 16:47:28 +0300

    reading props in event handler

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,30 @@
 import React from "react";
+import PropTypes from "prop-types";
+
+const AlertButton = ({ msg, children }) => {
+  return (
+    <button
+      onClick={() => {
+        alert(msg);
+      }}
+      style={{padding: "4px", margin: "4px"}}
+    >
+      {children}
+    </button>
+  );
+};
+
+AlertButton.propTypes = {
+  msg: PropTypes.string,
+  children: PropTypes.children,
+};
 
 const App = () => {
-  const handleClick = () => {
-    alert("You clicked me!")
-  }
   return (
-    <button onClick={handleClick}>
-      Click me
-    </button>
+    <>
+      <AlertButton msg="Playing">Play Movie</AlertButton>
+      <AlertButton msg="Uploading">Upload Image</AlertButton>
+    </>
   );
 };
 
