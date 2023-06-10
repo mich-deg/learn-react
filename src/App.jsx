1a19b9f72946ffd475c2e5f07370323f218ef0b5
Michael <lemichael1215@gmail.com>
2023-06-10 18:45:37 +0300

    JS Object state as read-only

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,37 @@
 import React, { useState } from "react";
-import { sculptureList } from "../data";
-
-import Form from "./component/Form";
-import Form2 from "./component/Form2";
 
 const App = () => {
-  const [index, setIndex] = useState(0);
-  const [showMore, setShowMore] = useState(false);
-
-  function handleClick() {
-    setIndex(index + 1);
-  }
-
-  function handleMoreClick() {
-    setShowMore(!showMore);
-  }
-  let sculpture = sculptureList[index];
+  const [position, setPosition] = useState({
+    x:0,
+    y:0
+  })
   return (
-    <>
-      <button onClick={handleClick}>Next</button>
-      <h2>
-        <i>{sculpture.name}</i> by {sculpture.artist}
-      </h2>
-      <h3>
-        ({index + 1} of {sculptureList.length} )
-      </h3>
-      <button onClick={handleMoreClick}>
-        {showMore ? "Hide" : "Show"} details
-      </button>
-      {showMore && <p>{sculpture.description}</p>}
-      <img src={sculpture.url} alt={sculpture.alt} />
-      <Form />
-      <Form2 />
-    </>
-  );
+
+    <div onPointerMove={
+      e => {
+        setPosition({
+          x: e.clientX,
+          y: e.clientY
+        })
+      }
+    } style={{
+      position: 'relative',
+      width: '100vw',
+      height: '100vh',
+    }}>
+      <div style={{
+        position: 'absolute',
+        backgroundColor: 'blue',
+        borderRadius: '50%',
+        transform: ` translate(${position.x}px, ${position.y}px) `,
+        left: -10,
+        top: -10,
+        width: 20,
+        height: 20
+      }} />
+  </div>
+  )
+
 };
 
 export default App;
