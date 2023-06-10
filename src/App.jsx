e0dd7535a83dc7e93dc5ddd21b4615a063b081c0
Michael <lemichael1215@gmail.com>
2023-06-10 19:23:25 +0300

    Copying objects with the spread syntax

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,40 @@
 import React, { useState } from "react";
+import Form3 from "./component/Form3";
 
 const App = () => {
   const [position, setPosition] = useState({
-    x:0,
-    y:0
-  })
+    x: 0,
+    y: 0,
+  });
   return (
-
-    <div onPointerMove={
-      e => {
+    <div
+      onPointerMove={(e) => {
         setPosition({
           x: e.clientX,
-          y: e.clientY
-        })
-      }
-    } style={{
-      position: 'relative',
-      width: '100vw',
-      height: '100vh',
-    }}>
-      <div style={{
-        position: 'absolute',
-        backgroundColor: 'blue',
-        borderRadius: '50%',
-        transform: ` translate(${position.x}px, ${position.y}px) `,
-        left: -10,
-        top: -10,
-        width: 20,
-        height: 20
-      }} />
-  </div>
-  )
-
+          y: e.clientY,
+        });
+      }}
+      style={{
+        position: "relative",
+        width: "100vw",
+        height: "100vh",
+      }}
+    >
+      <div
+        style={{
+          position: "absolute",
+          backgroundColor: "blue",
+          borderRadius: "50%",
+          transform: ` translate(${position.x}px, ${position.y}px) `,
+          left: -10,
+          top: -10,
+          width: 20,
+          height: 20,
+        }}
+      />
+      <Form3 />
+    </div>
+  );
 };
 
 export default App;
---
+++ src/component/Form3.jsx
@@ -0,0 +1,52 @@
+import React, { useState } from "react";
+
+const Form3 = () => {
+  const [person, setPerson] = useState({
+    firstName: "John",
+    lastName: "Doe",
+    email: "john.doe@sculpture.com",
+  });
+
+  function handleFirstNameChange (e) {
+    setPerson({
+        ...person, 
+        firstName: e.target.value
+    })
+  } 
+
+  function handleLastNameChange (e){
+    setPerson({
+        ...person,
+        lastName: e.target.value
+    })
+  }
+
+  function handleEmailChange(e) {
+    setPerson({
+      ...person,
+      email: e.target.value
+    });
+  }
+
+  return (
+    <>
+      <label htmlFor="">
+        First name:
+        <input value={person.firstName} onChange={handleFirstNameChange} />
+      </label>
+      <label htmlFor="">
+        Last name:
+        <input value={person.lastName} onChange={handleLastNameChange} />
+      </label>
+      <label htmlFor="">
+        Email:
+        <input value={person.email} onChange={handleEmailChange} />
+      </label>
+      <p>
+        {person.firstName} {person.lastName} ({person.email})
+      </p>
+    </>
+  );
+};
+
+export default Form3;
